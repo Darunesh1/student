eff7a66aefbc64ae5fa1ab6f8c64f347e4c5f4f3
Darunesh1 <daru22018.it@rmkec.ac.in>
2024-05-03 14:49:10 +0530

    added orgin in cotroller allows for cross platform intercation

--- src/main/java/com/reactboot/student/controller/StudentController.java
+++ src/main/java/com/reactboot/student/controller/StudentController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/student")
+@CrossOrigin
 public class StudentController {
     @Autowired
     private StudentService studentService;
