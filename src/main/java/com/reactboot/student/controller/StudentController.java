b262309aeac1ae5fe01463abf8bc1af8ecdb8c96
Darunesh1 <daru22018.it@rmkec.ac.in>
2024-05-03 10:53:54 +0530

    added a getAll method in controller and a method getAllStudents in services and checked it using postman

--- src/main/java/com/reactboot/student/controller/StudentController.java
+++ src/main/java/com/reactboot/student/controller/StudentController.java
@@ -5,10 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.reactboot.student.model.Student;
 import com.reactboot.student.service.StudentService;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -24,6 +29,14 @@ public class StudentController {
         studentService.saveStudent(student);
         return "New student is added";
     }
+
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents()
+    {
+        return studentService.getAllStudents();
+    }
+    
     
 
 
--- src/main/java/com/reactboot/student/service/StudentService.java
+++ src/main/java/com/reactboot/student/service/StudentService.java
@@ -1,9 +1,12 @@
 package com.reactboot.student.service;
 
+import java.util.List;
+
 import com.reactboot.student.model.Student;
 
 public interface StudentService {
 
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 
 }
